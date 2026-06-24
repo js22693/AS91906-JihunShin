bebbac82c39035eb42b9003509473d9c5e32cdbf
js22693 <js22693@my.westlake.school.nz>
2026-06-24 12:50:32 +1200

    24/06/2026

--- 91906.py
+++ 91906.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+import json
 
 
 class Food:
@@ -9,7 +10,17 @@ class Food:
         self.food_type = food_type
         self.storage = storage
         self.region = region
-        
+
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "expiry": self.expiry,
+            "food_type": self.food_type,
+            "storage": self.storage,
+            "region": self.region
+        }
+
 
 class FridgeApp:
 
@@ -27,6 +38,7 @@ class FridgeApp:
         self.refrigerated_count = 0
 
         self.create_gui()
+        self.load_foods()
 
     def create_info_box(self, parent):
 
@@ -73,16 +85,17 @@ class FridgeApp:
             anchor="w"
         )
         storage.pack(fill="x", padx=10)
+        delete_button = tk.Button(frame,text="Delete")
 
-        delate = tk.Button(frame, text="Delate")
-        delate.pack(fill="x", pady=20, padx=30)
+        delete_button.pack(pady=10)
 
         return {
             "name": name,
             "expiry": expiry,
             "type": food_type,
             "storage": storage,
-            "delate_button": delate
+            "delete_button": delete_button
+
         }
 
 
@@ -96,7 +109,7 @@ class FridgeApp:
         self.name_entry.grid(row=0, column=1)
 
 
-        tk.Label(top_frame,text="Type:",bg="#d9d9d9").grid(row=2, column=0)
+        tk.Label(top_frame,text="Type:",bg="#d9d9d9").grid(row=1,column=0)
         self.type_box = ttk.Combobox(
             top_frame,
             values=[
@@ -109,71 +122,52 @@ class FridgeApp:
             state="readonly",
             width=37
         )
+        self.type_box.grid(row=1,column=1)
 
 
-        
-        tk.Label(top_frame,text="Expiry Day:",bg="#d9d9d9").grid(row=1, column=0)
-        self.expiry_day_box = ttk.Combobox(
-            top_frame,
-            values=[
-                1,
-                2,
-                3,
-                4,
-                5
-            ],
-            state="readonly",
-            width=17
+        self.expiry_label = tk.Label(top_frame,text="Expiry Date:",bg="#d9d9d9")
+        self.expiry_label.grid(row=2,column=0)
+        self.expiry_frame = tk.Frame(top_frame,bg="#d9d9d9")
+        self.expiry_frame.grid(row=2,column=1)
+
+        #Day
+        self.day_box = ttk.Combobox(
+            self.expiry_frame,
+            values=[str(i).zfill(2) for i in range(1, 32)],
+            width=9,
+            state="readonly"
         )
-        self.expiry_day_box.grid(row=1, column=0)
-        self.type_box.grid(row=2, column=1)
+        self.day_box.pack(side="left", padx=2)
 
-
-        tk.Label(top_frame,bg="#d9d9d9").grid(row=1, column=2)
-        self.expiry_month_box = ttk.Combobox(
-            top_frame,
-            values=[
-                1,
-                2,
-                3,
-                4,
-                5
-            ],
-            state="readonly",
-            width=10
+        #Month
+        self.month_box = ttk.Combobox(
+            self.expiry_frame,
+            values=[str(i).zfill(2) for i in range(1, 13)],
+            width=9,
+            state="readonly"
         )
-        self.expiry_month_box.grid(row=1, column=1)
-        self.type_box.grid(row=2, column=1)
+        self.month_box.pack(side="left", padx=2)
 
 
+        #Day until Expiry
+        self.days_frame = tk.Frame(top_frame,bg="#d9d9d9")
+        self.days_entry = tk.Entry(self.days_frame,width=40)
+        self.days_entry.pack(side="left")
 
-        tk.Label(top_frame,bg="#d9d9d9").grid(row=1, column=2)
-        self.expiry_year_box = ttk.Combobox(
-            top_frame,
+        #Year
+        from datetime import datetime
+        current_year = datetime.now().year
+        self.year_box = ttk.Combobox(
+            self.expiry_frame,
             values=[
-                2026,
-                2027,
-                2028,
-                2029
+                str(year)
+                for year in range(current_year, current_year + 6)
             ],
-            state="readonly",
-            width=10
+            width=10,
+            state="readonly"
         )
-        self.expiry_year_box.grid(row=1, column=2)
-        self.type_box.grid(row=2, column=1)
-
-
-
-
-
-        tk.Label(top_frame,text="Expiry date:",bg="#d9d9d9").grid(row=0, column=0, padx=10, pady=5)
-        self.expiry_date_vage = tk.Entry(top_frame,width=40)
-        self.expiry_date_vage.grid(row=0, column=1)
-
-
-
-
-
+        self.year_box.pack(side="left", padx=2)
+        self.type_box.bind("<<ComboboxSelected>>",self.change_expiry_input)
 
 
         tk.Label(top_frame,text="Storage:",bg="#d9d9d9").grid(row=3, column=0)
@@ -244,29 +238,49 @@ class FridgeApp:
             4: self.box4
         }
 
+
+
+    def change_expiry_input(self, event=None):
+        selected_type = self.type_box.get()
+
+        if selected_type in ["Fruits", "Vegetable"]:
+            self.expiry_label.config(text="Day(s) Until Expiry:")
+            self.expiry_frame.grid_remove()
+            self.days_frame.grid(row=2,column=1,sticky="w")
+        else:
+            self.expiry_label.config(text="Expiry Date:")
+            self.days_frame.grid_remove()
+            self.expiry_frame.grid(row=2,column=1,sticky="w")
+
     def add_food(self):
         name = self.name_entry.get()
-
-        if food_type == "Vegetable" or "Fruits":
-            self.expiry_date_vage
-        else:
-            expiry_day_box = self.expiry_day_box.get()
-            expiry_month_box = self.expiry_month_box.get()
-            expiry_year_box = self.expiry_year_box.get()
-            expiry = f"{expiry_day_box}-{expiry_month_box}-{expiry_year_box}"
-
-
         food_type = self.type_box.get()
         storage = self.storage_box.get()
-            
+
+        from datetime import datetime, timedelta
+        if food_type in ["Fruits", "Vegetable"]:
+            days = self.days_entry.get()
+            if not days.isdigit():
+                return
+            expiry_date = datetime.now() + timedelta(days=int(days))
+            expiry = expiry_date.strftime("%d/%m/%Y")
+
+        else:
+            day = self.day_box.get()
+            month = self.month_box.get()
+            year = self.year_box.get()
+            if not day or not month or not year:
+                return
+            expiry = f"{day}/{month}/{year}"
+    
+
+
 
         if not name:
             return
         if not food_type:
             return
         if not storage:
-            return
-        if not expiry_day_box or expiry_month_box or expiry_year_box:
             return
         #It Will change icons
         colors = {
@@ -316,7 +330,7 @@ class FridgeApp:
             expiry,
             food_type,
             storage,
-            region,
+            region
         )
 
         rect = self.canvas.create_rectangle(
@@ -324,7 +338,7 @@ class FridgeApp:
             y,
             x + 30,
             y + 30,
-            fill=colors[food_type],
+            fill=self.get_food_color(food_type),
             outline="black"
         )
 
@@ -335,22 +349,19 @@ class FridgeApp:
             "<Button-1>",
             lambda e, r=rect: self.show_info(r)
         )
-    
-    """
-    def show_message(self, text, color="red"):
-        self.message_label.config(text=text, fg=color)
-    """
-
+        self.save_foods()
 
     def show_info(self, rect):
         self.selected_rect = rect
         food = self.food_objects[rect]
-        box = self.region_boxes[food.region]
+        box = self.region_boxes[
+            food.region
+        ]
         box["name"].config(text=f"Name: {food.name}")
         box["expiry"].config(text=f"Expiry: {food.expiry}")
         box["type"].config(text=f"Type: {food.food_type}")
         box["storage"].config(text=f"Storage: {food.storage}")
-        box["delate_button"].config(command=lambda r=rect: self.delete_food(r))
+        box["delete_button"].config(command=lambda r=rect: self.delete_food(r))
 
     def delete_food(self, rect):
 
@@ -362,6 +373,8 @@ class FridgeApp:
         self.canvas.delete(rect)
 
         del self.food_objects[rect]
+        self.reorganize_foods()
+        self.save_foods()
 
         box = self.region_boxes[food.region]
 
@@ -371,6 +384,130 @@ class FridgeApp:
         box["storage"].config(text="Storage:")
 
         self.selected_rect = None
+
+
+    def save_foods(self):
+        data = []
+        for food in self.food_objects.values():
+            data.append(
+                food.to_dict()
+            )
+        with open(
+            "foods.json",
+            "w"
+        ) as file:
+            json.dump(
+                data,
+                file,
+                indent=4
+            )
+    
+
+    def reorganize_foods(self):
+        foods = list(self.food_objects.values())
+        for item in list(self.food_objects.keys()):
+            self.canvas.delete(item)
+
+        self.food_objects.clear()
+        self.frozen_count = 0
+        self.refrigerated_count = 0
+        for food in foods:
+            if food.storage == "Frozen":
+                count = self.frozen_count
+                x = 110 + (count % 6) * 55
+                y = 90 + (count // 6) * 55
+                self.frozen_count += 1
+            else:
+                count = self.refrigerated_count
+                if count < 12:
+                    x = 110 + (count % 6) * 55
+                    y = 270 + (count // 6) * 55
+                else:
+                    count -= 12
+                    x = 110 + (count % 6) * 55
+                    y = 450 + (count // 6) * 55
+                self.refrigerated_count += 1
+            rect = self.canvas.create_rectangle(
+                x,
+                y,
+                x + 30,
+                y + 30,
+                fill=self.get_food_color(food.food_type),
+                outline="black"
+            )
+            self.food_objects[rect] = food
+            self.canvas.tag_bind(
+                rect,
+                "<Button-1>",
+                lambda e, r=rect: self.show_info(r)
+            )
+
+
+    def get_food_color(self, food_type):
+        colors = {
+            "Dairy": "#5DADE2",
+            "Meat": "#EC7063",
+            "Vegetable": "#58D68D",
+            "Fruits": "#F4D03F",
+            "Frozen Food": "#2A98E2"
+        }
+        return colors[food_type]
+    
+
+    def create_food_from_object(self, food):
+
+        if food.storage == "Frozen":
+            count = self.frozen_count
+            x = 110 + (count % 6) * 55
+            y = 90 + (count // 6) * 55
+            self.frozen_count += 1
+        else:
+            count = self.refrigerated_count
+            if count < 12:
+                x = 110 + (count % 6) * 55
+                y = 270 + (count // 6) * 55
+            else:
+                count -= 12
+                x = 110 + (count % 6) * 55
+                y = 450 + (count // 6) * 55
+            self.refrigerated_count += 1
+        rect = self.canvas.create_rectangle(
+            x,
+            y,
+            x + 30,
+            y + 30,
+            fill=self.get_food_color(food.food_type),
+            outline="black"
+        )
+        self.food_objects[rect] = food
+        self.canvas.tag_bind(
+            rect,
+            "<Button-1>",
+            lambda e, r=rect: self.show_info(r)
+        )
+
+
+
+    def load_foods(self):
+        try:
+            with open(
+                "foods.json",
+                "r"
+            ) as file:
+                data = json.load(file)
+        except:
+            return
+
+        for item in data:
+            food = Food(
+                item["name"],
+                item["expiry"],
+                item["food_type"],
+                item["storage"],
+                item["region"]
+            )
+            self.create_food_from_object(food)
+
 
 
 root = tk.Tk()
